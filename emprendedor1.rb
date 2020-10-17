b50b8707f756245195423625c00f77a854c740b7
xhispams <locopor.m3ry@gmail.com>
2020-10-17 13:42:10 -0300

    change in variable rate, from integer to float, precio_venta ( in case it is entered with decimals for being uf or dollar)

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -6,13 +6,13 @@
 # Se agrega validación para que indique cuando existe $ 0 de ulilidad, ya que existen gastos asociados 
 # ruby emprendedor1.rb 50 1000 50000
 # Ingrese el precio, el número de usuarios y los gastos. El programa calcula las utilidades.
-precio_venta = ARGV[0].to_i 
+precio_venta = ARGV[0].to_f
 usuarios = ARGV[1].to_i 
 gastos = ARGV[2].to_i
 utilidad =precio_venta*usuarios-gastos
 if utilidad >= 0
     utilidad =(precio_venta*usuarios-gastos)-utilidad*0.35
-    puts "La Utilidad obtenida es de $ #{utilidad}, con precio venta del producto de $ #{precio_venta}, #{usuarios} usuarios al año, y gastos que ascienden a $ #{gastos}"
+    puts "La Utilidad obtenida es de $ #{utilidad.round(2)}, con precio venta del producto de $ #{precio_venta}, #{usuarios} usuarios al año, y gastos que ascienden a $ #{gastos}"
 elsif utilidad < 0 and utilidad !=0
     puts "La Utilidad obtenida es menor a $ 0 para los datos ingresados"
 end

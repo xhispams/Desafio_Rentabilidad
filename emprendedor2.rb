e41d750364c1034bb9a52e7143140a7a5144236a
xhispams <locopor.m3ry@gmail.com>
2020-10-17 13:57:14 -0300

    cosmetic changes in messages

--- emprendedor2.rb
+++ emprendedor2.rb
@@ -1,23 +1,21 @@
 # @author Álvaro Moya Santana
-# precio_venta : El precio del producto que planea venderse (por ejemplo 50 dólares)
-# usuarios : Cantidad de Usuarios en el año
-# utilidad : Las utilidades se calculan como precio_venta * usuarios -gastos
-# impuestos : Los impuestos son aplicados a las utilidades son el 35% y solo aplican si es positivo
-# Se agrega validación para que indique cuando existe $ 0 de ulilidad, ya que existen gastos asociados 
-# ruby emprendedor1.rb 50 1000 50000
-precio = ARGV[0].to_i 
+#
+precio = ARGV[0].to_f
 usuarios = ARGV[1].to_i 
 usuarios_premium = ARGV[2].to_i 
 usuarios_gratis = ARGV[3].to_i
-gastos= ARGV[4].to_i
+gastos= ARGV[4].to_f
 
+usuarios_calculo = usuarios + usuarios_premium
 num_usuarios = usuarios_premium * 2 + usuarios
 utilidad = num_usuarios*precio - gastos
 if utilidad >= 0
    utilidad = utilidad*0.65
-   puts utilidad
+   puts "La Utilidad calculada es de $ #{utilidad.round(2)} y la cantidad de usuarios tomados para este cálculo son #{usuarios_calculo}"
+else utilidad < 0
+   puts "La Utilidad calculada es menor a $ 0, no se alcanzan a cubrir los gastos"
 end
- puts usuario
- puts usu_premium
- puts precio
- puts gastos
+ puts "La cantidad de usuarios ingresada es de #{usuarios}"
+ puts "La cantidad de usuarios premiun es de #{usuarios_premium}"
+ puts "El precio ingresado es de $ #{precio}"
+ puts "Los gastos ingresados son $ #{gastos}"
